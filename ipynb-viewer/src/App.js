c3c395f14fb6ce1b1a6d87f451827f8ff346133e
phcreery <phcreery@gmail.com>
2020-12-16 23:33:25 -0600

    format with prettier

--- ipynb-viewer/src/App.js
+++ ipynb-viewer/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import JupViewer from './JupViewer'
 
-const ipynb = require('./BenchmarkNotebook.ipynb')
+const ipynb = require('./Transformation2D.ipynb')
 
 class App extends React.Component {
   render() {
@@ -13,8 +13,9 @@ class App extends React.Component {
           title="Jupyter as a Blog!"
           subtitle="I've always wanted to publish my jupyter notebooks as blogs. Finally I can."
           // coverImg="https://notionpress.com/blog/wp-content/uploads/2018/06/Cover-design.png"
-          // file={ipynb}
-          file="https://raw.githubusercontent.com/jakevdp/PythonDataScienceHandbook/master/notebooks/00.00-Preface.ipynb"
+          file={ipynb}
+          // file="https://raw.githubusercontent.com/jakevdp/PythonDataScienceHandbook/master/notebooks/00.00-Preface.ipynb"
+          // file="https://raw.githubusercontent.com/hpcreery/devtwins.js/blob/master/server/public/Projects/Jupyter%20Notebook/Transformation2D.ipynb"
           // file="https://raw.githubusercontent.com/fastai/course-v3/master/nbs/dl1/00_notebook_tutorial.ipynb"
         />
       </div>
